e2386c95d96dcc831a79b5917a316229eb4fedf8
“Mbaye <seckm105@gmail.com>
2023-11-09 12:24:59 +0000

    recup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
-} from "firebase/auth";
+} from 'firebase/auth';
 import {
   getFirestore,
   collection,
@@ -12,22 +12,24 @@ import {
   doc,
   getDoc,
   onSnapshot,
-} from "firebase/firestore";
+  serverTimestamp,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSRo2EZwo5LQIO75FevIBvEKbDD61HNuY",
-  authDomain: "validation-atelier-js.firebaseapp.com",
-  databaseURL: "https://validation-atelier-js-default-rtdb.firebaseio.com",
-  projectId: "validation-atelier-js",
-  storageBucket: "validation-atelier-js.appspot.com",
-  messagingSenderId: "466332062090",
-  appId: "1:466332062090:web:ffbe45ef4a7371a7b5b873",
+  apiKey: 'AIzaSyCSRo2EZwo5LQIO75FevIBvEKbDD61HNuY',
+  authDomain: 'validation-atelier-js.firebaseapp.com',
+  databaseURL: 'https://validation-atelier-js-default-rtdb.firebaseio.com',
+  projectId: 'validation-atelier-js',
+  storageBucket: 'validation-atelier-js.appspot.com',
+  messagingSenderId: '466332062090',
+  appId: '1:466332062090:web:ffbe45ef4a7371a7b5b873',
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app); 
+const db = getFirestore(app);
 
-import './pagesJS/login'
-import './pagesJS/profil'
-import './pagesJS/inscription'
+import './pagesJS/login';
+import './pagesJS/profil';
+import './pagesJS/inscription';
+import './pagesJS/compta';
