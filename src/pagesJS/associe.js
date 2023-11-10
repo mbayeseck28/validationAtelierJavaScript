908a32d417aea20e2bdc4a30f32eaf7865d8e482
unknown <miss-ndeya>
2023-11-10 09:10:52 +0000

    ajout image associe

--- src/pagesJS/associe.js
+++ src/pagesJS/associe.js
@@ -1,49 +1,83 @@
-import {getFirestore, collection, onSnapshot, addDoc} from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+} from "firebase/firestore";
 
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-export function gestionAssocie() {
-    const db = getFirestore()
-
-// const entreprise = collection(db, "entreprise");
-const associe = collection(db, "associe");
-
-console.log(associe);
-onSnapshot(associe, (snapshot) =>{
-    let associe = [];
-    snapshot.docs.forEach((doc) => {
-        associe.push({...doc.data(), id: doc.id});
-    });
-    console.log(associe);
-    const logoEntreprise = document.querySelector('#logo-entreprise')
-    logoEntreprise.innerHTML = ""
-    associe.forEach((ins) => {
-        const div = document.createElement('div');
-        div.className = "col-lg-2 align-items-center"
-        div.innerHTML = `
-        <button type="button" class="btn btn-dark shadow color-text">
-            Logo 
-        </button>                                                            
-        <p >${ins.nom}</p>   
-        `
-        logoEntreprise.appendChild(div);
-    });
-})
+async function storageImage(file) {
+  const storage = getStorage();
+  const storageRef = ref(storage, "images/" + file.name);
+  await uploadBytes(storageRef, file);
+  const downloadURL = await getDownloadURL(storageRef);
+  return downloadURL;
 }
 
-export function ajouterAssocier(formAssocie) {
-    const db = getFirestore();
-    const associe = collection(db, "associe");
-  
-    addDoc(associe, {
+export async function ajouterAssocier(formAssocie) {
+  const db = getFirestore();
+  const associeCollection = collection(db, "associe");
+
+  try {
+    const imageURL = await storageImage(formAssocie.fileInput.files[0]);
+    console.log("imageURL", imageURL);
+    await addDoc(associeCollection, {
       nom: formAssocie.nomAss.value,
       coordonnee: formAssocie.coordonneeAss.value,
       email: formAssocie.emailAss.value,
-      logo: formAssocie.fileInput.value
-      
-    }).then(() => {
-        console.log(formAssocie.nomAss.value);
-        formAssocie.reset()
+      logo: imageURL,
     });
-  }
 
- 
+    alert("Associe ajouté avec succès !", imageURL);
+    formAssocie.reset();
+  } catch (error) {
+    console.error("Erreur lors de l'ajout de l'associe :", error);
+    alert("Erreur lors de l'ajout de l'associe :", error)
+  }
+}
+
+export function gestionAssocie() {
+  const db = getFirestore();
+
+  // const entreprise = collection(db, "entreprise");
+  const associe = collection(db, "associe");
+
+  // console.log(associe);
+  onSnapshot(associe, (snapshot) => {
+    let associe = [];
+    snapshot.docs.forEach((doc) => {
+      associe.push({ ...doc.data(), id: doc.id });
+    });
+    // console.log(associe);
+    const logoEntreprise = document.querySelector("#logo-entreprise");
+    logoEntreprise.innerHTML = "";
+    associe.forEach((ins) => {
+      const div = document.createElement("div");
+      div.className = "col-lg-2 align-items-center ";
+      div.innerHTML = `
+      <button class="btn btn-dark rounded-pill">
+        <img src="${ins.logo}" alt="Logo de l'associe" class="img rounded-pill">                                                           
+      </button>
+          <p>${ins.nom}</p>
+        `;
+      logoEntreprise.appendChild(div);
+    });
+  });
+}
+
+// export function ajouterAssocier(formAssocie) {
+//     const db = getFirestore();
+//     const associe = collection(db, "associe");
+
+//     addDoc(associe, {
+//       nom: formAssocie.nomAss.value,
+//       coordonnee: formAssocie.coordonneeAss.value,
+//       email: formAssocie.emailAss.value,
+//       logo: formAssocie.fileInput.value
+
+//     }).then(() => {
+//         console.log(formAssocie.nomAss.value);
+//         formAssocie.reset()
+//     });
+//   }
