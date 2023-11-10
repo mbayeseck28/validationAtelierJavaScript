a8f129bdf1e971694db242c5eed6c01f835c30f7
“Mbaye <seckm105@gmail.com>
2023-11-10 11:25:25 +0000

    reglage du sidebar

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { getAuth } from "firebase/auth";
+import { onAuthStateChanged } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 function deconnexion() {
-  const btnGreen = document.querySelector('.btnGreen')
-  btnGreen.addEventListener('click', function(){
+  const btnGreen = document.querySelector('.btnGreen');
+  btnGreen.addEventListener('click', function () {
     const auth = getAuth();
-    signOut(auth).then(() => {
+    signOut(auth)
+      .then(() => {
         console.log('Utilisateur déconnecté');
-
-    }).catch((error) => {
-        window.location.href = "../../pages/auth/login/login.html";
+      })
+      .catch((error) => {
+        window.location.href = '../../pages/auth/login/login.html';
         console.error('Erreur lors de la déconnexion :', error);
-    });
-  })  
+      });
+  });
 }
-
 
 // onAuthStateChanged(auth, (user) => {
 //     if (!user) {
@@ -22,28 +22,24 @@ function deconnexion() {
 //     }
 // });
 
-
-
-
-
 var pageName = window.location.pathname.split('/').pop();
 
 if (pageName === 'inscription.html') {
   import('./pagesJS/inscription');
 } else if (pageName === 'login.html') {
   import('./pagesJS/login');
-}else if (pageName === 'accueil.html') {
-  import('./pagesJS/sidebar');  
-}else if (pageName === 'profil.html') {
+} else if (pageName === 'accueil.html') {
+  import('./pagesJS/sidebar');
+} else if (pageName === 'profil.html') {
   import('./pagesJS/sidebar');
   import('./pagesJS/profil');
 } else if (pageName === 'comptabilite.html') {
-  import('./pagesJS/sidebar');
+  // import('./pagesJS/sidebar');
   import('./pagesJS/compta');
-}else if (pageName === 'personnel.html') {
+} else if (pageName === 'personnel.html') {
   import('./pagesJS/sidebar');
   import('./pagesJS/personnel.js');
-}else if (pageName === 'professeur.html') {
+} else if (pageName === 'professeur.html') {
   import('./pagesJS/sidebar');
   import('./pagesJS/proff.js');
 }
