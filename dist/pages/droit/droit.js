7291691e553213b28d54fdc6b8da9f2905eb691a
BAMoustapha <bam480836@gmail.com>
2023-11-14 11:53:41 +0000

    ba

--- dist/pages/droit/droit.js
+++ dist/pages/droit/droit.js
@@ -1,3 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, onLog } from 'firebase/app';
+// Importation des  services
+import {
+  addDoc,
+  collection,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  serverTimestamp,
+} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCSRo2EZwo5LQIO75FevIBvEKbDD61HNuY',
+  authDomain: 'validation-atelier-js.firebaseapp.com',
+  projectId: 'validation-atelier-js',
+  storageBucket: 'validation-atelier-js.appspot.com',
+  messagingSenderId: '466332062090',
+  appId: '1:466332062090:web:ffbe45ef4a7371a7b5b873',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+
+
 document.getElementById("bouton").addEventListener("click", (e) => {
     
     const one = document.querySelector(".one");
