bd8556ed23404922cede2d292635c28fbf6fbe61
unknown <miss-ndeya>
2023-11-15 00:01:10 +0000

    reglage

--- src/pagesJS/accueil.js
+++ src/pagesJS/accueil.js
@@ -58,6 +58,37 @@ onSnapshot(eleve, (snapshot) => {
   effectif3.innerHTML = effectifClass3 + " élèves";
 });
 
+
+const btnAfficherPaiements = document.getElementById('btnAfficherPaiements');
+const selectMois = document.getElementById('selectMois');
+
+btnAfficherPaiements.addEventListener('click', () => {
+  const moisSelectionne = selectMois.value;
+
+  onSnapshot(certiesRef2, (snapshot) => {
+    let certiesRef2 = [];
+    snapshot.docs.forEach((doc) => {
+      certiesRef2.push({ ...doc.data(), id: doc.id });
+    });
+    certiesRef2.sort((a, b) => b.dateDajout - a.dateDajout);
+
+    let PaiementsEffec6 = certiesRef2.filter((utili) => utili.classe === '6ème' && utili.mois === moisSelectionne);
+    let PaiementsEffec5 = certiesRef2.filter((utili) => utili.classe === '5ème' && utili.mois === moisSelectionne);
+    let PaiementsEffec4 = certiesRef2.filter((utili) => utili.classe === '4ème' && utili.mois === moisSelectionne);
+    let PaiementsEffec3 = certiesRef2.filter((utili) => utili.classe === '3ème' && utili.mois === moisSelectionne);
+
+    paiement6.innerHTML =
+    Math.round((PaiementsEffec6.length / effectifClass6) * 100) + "%";
+  paiement5.innerHTML =
+    Math.round((PaiementsEffec5.length / effectifClass5) * 100) + "%";
+  paiement4.innerHTML =
+    Math.round((PaiementsEffec4.length / effectifClass4) * 100) + "%";
+  paiement3.innerHTML =
+    Math.round((PaiementsEffec3.length / effectifClass3) * 100) + "%";
+  });
+});
+
+
 onSnapshot(certiesRef2, (snapshot) => {
   let certiesRef2 = [];
   snapshot.docs.forEach((doc) => {
@@ -66,16 +97,16 @@ onSnapshot(certiesRef2, (snapshot) => {
   certiesRef2.sort((a, b) => b.dateDajout - a.dateDajout);
   console.log(certiesRef2);
   let PaiementsEffec6 = certiesRef2.filter(
-    (utili) => utili.classe === "6ème" && utili.mois === "octobre"
+    (utili) => utili.classe === "6ème" && utili.mois === "novembre"
   );
   let PaiementsEffec5 = certiesRef2.filter(
-    (utili) => utili.classe === "5ème" && utili.mois === "octobre"
+    (utili) => utili.classe === "5ème" && utili.mois === "novembre"
   );
   let PaiementsEffec4 = certiesRef2.filter(
-    (utili) => utili.classe === "4ème" && utili.mois === "octobre"
+    (utili) => utili.classe === "4ème" && utili.mois === "novembre"
   );
   let PaiementsEffec3 = certiesRef2.filter(
-    (utili) => utili.classe === "3ème" && utili.mois === "octobre"
+    (utili) => utili.classe === "3ème" && utili.mois === "novembre"
   );
 
   paiement6.innerHTML =
