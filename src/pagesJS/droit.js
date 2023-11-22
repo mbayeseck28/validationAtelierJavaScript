cca8993d5125e46af6177996819c5c33bb0b96d0
BAMoustapha <bam480836@gmail.com>
2023-11-22 13:32:45 +0000

    ba

--- src/pagesJS/droit.js
+++ src/pagesJS/droit.js
@@ -31,64 +31,37 @@ const db = getFirestore(app);
 const contenuRef = doc(db, 'droit', 'mZ2VxP5Ea2aAzJlzN9QV');
 
 
-// function loadContentFromFirestore() {
-//   return new Promise((resolve, reject) => {
-//     getDoc(contenuRef)
-//       .then((docSnapshot) => {
-//         if (docSnapshot.exists()) {
-//           // Parsez les données dans un format utilisable par Quill
-//           const data = docSnapshot.data().contenu;
-//           console.log("Données récupérées depuis Firestore :", data);
-          
-//           document.getElementById('editor-container').innerHTML = data;
-          
-          
-//           // Chargez les données dans l'éditeur Quill
-//           try {
-//             if (data) {
-//               quill.setContents(data);
-//               resolve();
-//             } else {
-//               console.log("Aucun document trouvé!");
-//               reject();
-//             }
-//           } catch (error) {
-//             console.error("Erreur lors de l'analyse du contenu JSON:", error);
-//             reject();
-//           }
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Erreur lors de la récupération du document Firestore:", error);
-//         reject();
-//       });
-//   });
-// }
 
-// // Attendez que la promesse soit résolue avant d'exécuter le reste du code
-// loadContentFromFirestore()
-//   .then(() => {
-//     // Mettez en place l'événement text-change pour détecter les modifications dans l'éditeur Quill
-    
-//     // quill.on('text-change', function (delta, oldDelta, source) {
-//     //   if (source === 'user') {
-//     //     change = change.compose(delta);
-//     //     sauvegarderContenuDansFirestore();
-//     //   }
-//     // });
-//   })
-//   .catch(() => {
-//     // Ce bloc sera exécuté en cas d'échec du chargement du contenu depuis Firestore
-//     console.log("Le chargement du contenu depuis Firestore a échoué.");
-//   });
+document.addEventListener("DOMContentLoaded", function () {
+  // Charger le contenu depuis Firestore dès que la page est prête
+  getDataFromFirestore()
+});
 
-//   document.addEventListener("DOMContentLoaded", function () {
-//       loadContentFromFirestore();
-//   });
+function getDataFromFirestore() {
+  console.log("Tentative de récupération du contenu depuis Firestore...");
+  console.log("ContenuRef :", contenuRef);
+
+  getDoc(contenuRef)
+    .then((doc) => {
+      console.log("Réponse de Firestore :", doc);
+      if (doc.exists()) {
+        const data = doc.data();
+        console.log("Données du document :", data);
+        // Mettez à jour le contenu de l'éditeur avec les données de Firestore
+        document.getElementById('editor-container').innerHTML = data.contenu;
+        console.log("Contenu mis à jour :", data.contenu);
+      } else {
+        console.log("Aucun document trouvé !");
+      }
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la récupération du document depuis Firestore:", error);
+    });
+}
 
 function sauvegarderContenuDansFirestore() {
   
-  const contenuDiv = document.getElementById('editor-container').textContent;
+  const contenuDiv = document.getElementById('editor-container').innerHTML;
 
   console.log("Contenu à sauvegarder :", contenuDiv);
 
@@ -96,7 +69,7 @@ function sauvegarderContenuDansFirestore() {
 
 
   // Save the content to Firestore
-  setDoc(contenuRef, {
+  return setDoc(contenuRef, {
     contenu: contenuDiv,
     timestamp: serverTimestamp(),
   })
@@ -108,4 +81,6 @@ function sauvegarderContenuDansFirestore() {
     });
 }
 
-document.getElementById("modif").addEventListener("click", sauvegarderContenuDansFirestore);
+document.getElementById("modif").addEventListener("click",  () => {
+  sauvegarderContenuDansFirestore();
+})
